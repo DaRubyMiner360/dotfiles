678309f9dd7321f79599261bbd8368a144bd7cc3
DevReaper0 <devreaper0@gmail.com>
2024-10-26 18:26:18 -0500

    Added Kvantum and fixed some new bugs.

--- .config/hypr/scripts/special_workspace.sh
+++ .config/hypr/scripts/special_workspace.sh
@@ -1,4 +1,4 @@
-if [ $(hyprctl activewindow -j | jq .workspace.name) = '"special"' ]; then
+if [[ $(hyprctl activewindow -j | jq .workspace.name) == '"special:'* ]]; then
   hyprctl dispatch movetoworkspacesilent $(hyprctl activeworkspace -j | jq .id)
 else
   hyprctl dispatch movetoworkspacesilent special
